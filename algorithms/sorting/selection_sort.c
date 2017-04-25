8352e99aa786391c53f27fe2b0ab090c7f26eee6
wendingp <wendingp@gmail.com>
2017-04-25 23:38:55 +0800

    Add selection sort implementation.

---
+++ algorithms/sorting/selection_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * Selection sort algorithm implementation.
+ */
+
+void swap(int *p1, int *p2) {
+    int tmp = *p1;
+    *p1 = *p2;
+    *p2 = tmp;
+}
+
+/* Main algorithm. */
+void selection_sort(int array[], int size) {
+    int i, j;
+    for (i = 0; i < size; ++i) {
+        int min_index = i;
+        for (j = i; j < size; ++j) {
+            if (array[j] < array[min_index]) {
+                min_index = j;
+            }
+        }
+        swap(array + i, array + min_index);
+    }
+}
+
+/* Test */
+int main() {
+    int array[] = {8, 2, 4, 8, 1, 22, 3, 44, 5, 6, 2, -1, 0};
+    int size = sizeof array / sizeof(int);
+    selection_sort(array, size);
+    int i;
+    for (i = 0; i < size; ++i) {
+        printf("%d%c", array[i], i == size - 1 ? '\n' : ' ');
+    }
+    return 0;
+}
