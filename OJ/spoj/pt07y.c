0c1e43f68b8071b3fb4e717a87d57aec8a50cc3a
wendingp <wendingp@gmail.com>
2017-07-19 14:05:27 +0800

    Adds NQueensII solution

--- OJ/spoj/pt07y.c
+++ OJ/spoj/pt07y.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int visited[10010];
+int n, m;
+int connected[10010][10010];
+
+void dfs(int v) {
+
+}
 
 void solve() {
-
+    scanf("%d %d ", &n, &m);
+    int i, u, v;
+    for (i = 0; i < m; ++i) {
+        scanf("%d %d ", &u, &v);
+        connected[u][v] = 1;
+    }
+    dfs(0);
+    for (i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            
+        }
+    }
 }
 
 int main() {
--- algorithms/shortest-path/dijkstra.c
+++ algorithms/shortest-path/dijkstra.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 enum {
-    INF = 987654321,  /* Shouldn't be too big, otherwise may cause overflow. */
+    INF = 9876543,  /* Shouldn't be too big, otherwise may cause overflow. */
     MAX_NUMBER_OF_NODES = 10000,
     VIRTUAL_ROOT = -1,
 };
@@ -12,42 +12,38 @@ int path_to[MAX_NUMBER_OF_NODES];
 int visited[MAX_NUMBER_OF_NODES];
 int dist[MAX_NUMBER_OF_NODES][MAX_NUMBER_OF_NODES];
 
-/* Inputs the distance of two connected vertices. */
-void input() {
-    n_nodes = 100;
-}
-
 void init_graph() {
+    n_nodes = 100;
     int i, j;
     for (i = 0; i < n_nodes; ++i) {
         visited[i] = 0;
-        for (j = i; j < n_nodes; ++j) {
+        path_to[i] = VIRTUAL_ROOT;
+        for (j = 0; j < n_nodes; ++j) {
             dist[i][j] = dist[j][i] = i == j ? 0 : INF;
         }
     }
 }
 
 void dijkstra_shortest_path(int root) {
+    visited[root] = 1;
+    int visiting = root, n_remained_nodes = n_nodes - 1;
     int i;
-    for (i = 0; i < n_nodes; ++i) {
-        path_to[i] = root;
-    }
-    path_to[root] = VIRTUAL_ROOT;
-    int visiting = root, remained_n_nodes = n_nodes - 1;
-    while (remained_n_nodes--) {
+    while (n_remained_nodes--) {
         for (i = 0; i < n_nodes; ++i) {
             if (!visited[i] && 
                 dist[root][visiting] + dist[visiting][i] < dist[root][i]) {
+                /* Updates if finds a shorter path. */
                 dist[root][i] = dist[root][visiting] + dist[visiting][i];
                 path_to[i] = visiting;
             }
         }
         int nearest_remained = -1, min_distance = INF;
+        /* Chooses the nearest-to-root remained node to be the next visiting. */ 
         for (i = 0; i < n_nodes; ++i) {
             if (!visited[i] && dist[root][i] < min_distance) {
                 nearest_remained = i;
                 min_distance = dist[root][i];
-            }
+            }   
         }
         visiting = nearest_remained;
         visited[visiting] = 1;
