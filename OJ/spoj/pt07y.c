80be332c6d736633d493524be335c656b8911311
wendingp <wendingp@gmail.com>
2017-07-19 14:27:37 +0800

    Adds is_a_tree solution.

--- OJ/spoj/pt07y.c
+++ OJ/spoj/pt07y.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* PT07Y: IS IT A TREE? */
+
 int visited[10010];
 int n, m;
-int connected[10010][10010];
+int has_edge[10010][10010];
 
 void dfs(int v) {
-
+    visited[v] = 1;
+    int i;
+    for (i = 0; i < n; ++i) {
+        if (has_edge[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
 }
 
 void solve() {
@@ -14,14 +22,20 @@ void solve() {
     int i, u, v;
     for (i = 0; i < m; ++i) {
         scanf("%d %d ", &u, &v);
-        connected[u][v] = 1;
+        has_edge[u-1][v-1] = 1;
+    }
+    if (n - m != 1) {
+        printf("NO\n");
+        return;
     }
     dfs(0);
     for (i = 0; i < n; ++i) {
         if (!visited[i]) {
-            
+            printf("NO\n");
+            return;
         }
     }
+    printf("YES\n");
 }
 
 int main() {
